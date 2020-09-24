97f4a55f37f3a69df12b2fb23a56d738c87f4fa5
EricNavar <ericnavar@ufl.edu>
2020-09-24 16:40:18 -0400

    Update text change "Society of Software Developers" to "Software Development and Design" add text saying the day and time of meetings

--- src/Components/HomePage/HomeBanner Section/HomeBanner.js
+++ src/Components/HomePage/HomeBanner Section/HomeBanner.js
@@ -6,8 +6,10 @@ function HomeBanner() {
     <section className={module_styles["home-banner-container"]}>
       <div className={module_styles["home-banner"]}>
         <h1 className={module_styles["home-banner-text"]}>
-          Society of Software Developers
+          Software Development and Design
         </h1>
+        <h2>Fall 2020 Meetings</h2>
+        <p style={{fontSize:18, marginTop:0}}>Wednesdays, 6:15pm over Zoom</p>
       </div>
     </section>
   );
