081c3f82170ead3a74e9128dd938f7021fdd87b0
Allan Lago <allanlago1@gmail.com>
2020-09-25 11:14:07 -0400

    Minor changes to document title and styling

--- src/Components/ContactPage/ContactPage.js
+++ src/Components/ContactPage/ContactPage.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ContactBanner from "./ContactBanner Section/ContactBanner";
 import module_styles from "./ContactPage.module.css";
 
 function ContactPage() {
+  useEffect(() => {
+    document.title = "Contact | Society of Software Developers";
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+
+    return () => {
+      document.title = "Society of Software Developers";
+    };
+  }, []);
+
   return (
     <main className={module_styles["Contact-Page"]}>
       <ContactBanner />
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AboutUs from "./AboutUs Section/AboutUs";
 import HomeBanner from "./HomeBanner Section/HomeBanner";
 import OfficersBanner from "./OfficersBanner Section/OfficersBanner";
 import module_styles from "./HomePage.module.css";
 
 function HomePage() {
+  useEffect(() => {
+    document.title = "Society of Software Developers";
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  }, []);
+
   return (
     <main className={module_styles["Home-page"]}>
       <HomeBanner />
