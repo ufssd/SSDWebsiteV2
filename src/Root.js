f67935d7a6cc4b96df0b741902a9314087cbbd81
Allan Lago <allanlago1@gmail.com>
2020-09-25 09:09:36 -0400

    Fixes header issue

--- src/Root.js
+++ src/Root.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import ContactPage from "./Components/ContactPage/ContactPage";
 import HomePage from "./Components/HomePage/HomePage";
 import EventsPage from "./screens/EventsPage";
+import ContactPage from "./Components/ContactPage/ContactPage";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 
 //https://reacttraining.com/react-router/web/guides/quick-start
 
 export default function Root() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/events" component={EventsPage} />
         <Route exact path="/contact" component={ContactPage} />
         <Redirect to="/" />
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }
