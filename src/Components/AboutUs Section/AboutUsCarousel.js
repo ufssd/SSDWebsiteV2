6b5bc88379c68da232cc81fa6d6b0091d031ea95
EricNavar <ericnavar@ufl.edu>
2020-10-31 18:59:09 -0400

    add another photo to caruousel fix typo in a description fix issue where height goes to zero while switching to another photo

--- src/Components/AboutUs Section/AboutUsCarousel.js
+++ src/Components/AboutUs Section/AboutUsCarousel.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Typography from '@material-ui/core/Typography';
 import SpeakerEventPhoto from './../../assets/InfoTechSpeakerEvent.jpg';
 import SSDGroupPhoto from './../../assets/SSDGroupPhoto.jpg';
+import TablingPhoto from './../../assets/Tabling.png';
 
 const useStyles = makeStyles((theme) => ({
   carouselContainer: {
@@ -16,14 +17,32 @@ const useStyles = makeStyles((theme) => ({
     color: 'white'
   },
   descriptionContainer: {
-    background: '#2a2a2a',
+    background: 'linear-gradient(90deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.60) 15%, rgba(0,0,0,0.60) 85%, rgba(0,0,0,0) 100%)',
     textAlign: 'center',
     paddingTop: 8,
     paddingBottom: 8,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%"
   },
   item: {
     maxWidth: "100vw",
     width: 450
+  },
+  aspectRatioBoxOuter: {
+    width: "inherit",
+    height: 0,
+    overflow: "hidden",
+    paddingTop: "75%",
+    position: "relative"
+  },
+  aspectRatioBoxInner: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%"
   },
   photo: {
     width: "inherit"
@@ -34,13 +53,18 @@ export default function AboutUsCarousel() {
   var items = [
     {
       name: "infotech",
-      description: "SSD after a tech talk from BridgeRock Datas",
+      description: "SSD after a tech talk from BridgeRock Data",
       photo: SSDGroupPhoto
     },
     {
       name: "group pic",
       description: "SSD members at InfoTech speaker series",
       photo: SpeakerEventPhoto
+    },
+    {
+      name: "tabling",
+      description: "Cool SSD members tabling 😎",
+      photo: TablingPhoto
     }
   ]
   return (
@@ -55,10 +79,12 @@ export default function AboutUsCarousel() {
 function Item(props) {
   const classes = useStyles();
   return (
-    <div className={classes.item}>
-      <img alt="" src={props.item.photo} className={classes.photo} />
-      <div className={classes.descriptionContainer}>
-        <Typography className={classes.description}>{props.item.description}</Typography>
+    <div className={classes.aspectRatioBoxOuter}>
+      <div className={classes.aspectRatioBoxInner}>
+        <img alt="" src={props.item.photo} className={classes.photo} />
+        <div className={classes.descriptionContainer}>
+          <Typography className={classes.description}>{props.item.description}</Typography>
+        </div>
       </div>
     </div>
   )
